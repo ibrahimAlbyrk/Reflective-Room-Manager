8291164a0cd390d177b3a6a6a3b753501936e822
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2023-08-17 23:57:36 +0300

    Network Manager development has been started

---
+++ ReflectiveRoomManager/Assets/_Project/Scripts/Runtime/NETWORK/Manager/REFLECTIVE_NetworkManager.cs
@@ -0,0 +1,11 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace REFLECTIVE.Runtime.NETWORK.Manager
+{
+    [AddComponentMenu("REFLECTIVE/Reflective Network Manager")]
+    public class REFLECTIVE_NetworkManager : NetworkManager
+    {
+        
+    }
+}
