f9e7994c65cfb0d8221dfe6e44b6616a08c0227a
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2023-08-17 23:51:35 +0300

    Network Spawn Utilities has been added

---
+++ ReflectiveRoomManager/Assets/_Project/Scripts/Runtime/NETWORK/Utilities/REFLECTIVE_NetworkSpawnUtilities.cs
@@ -0,0 +1,31 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace REFLECTIVE.Runtime.NETWORK.Utilities
+{
+    public static class REFLECTIVE_NetworkSpawnUtilities
+    {
+        public static GameObject SpawnObject(GameObject obj, Vector3 position = default, Quaternion rotation = default)
+        {
+            if (NetworkManager.singleton == null)
+            {
+                Debug.LogWarning("Network Manager is null", obj);
+                return null;
+            }
+
+            var instantObj = Object.Instantiate(obj, position, rotation);
+            NetworkServer.Spawn(instantObj);
+
+            return instantObj;
+        }
+
+        public static GameObject SpawnObjectForScene(Scene scene, GameObject obj, Vector3 position = default, Quaternion rotation = default)
+        {
+            var spawnedObj = SpawnObject(obj, position, rotation);
+            SceneManager.MoveGameObjectToScene(spawnedObj, scene);
+
+            return spawnedObj;
+        }
+    }
+}
