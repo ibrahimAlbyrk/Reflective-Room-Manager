173f94c547487af85e70ca0bd75bdaf5be91aa39
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2023-11-01 16:44:49 +0300

    Player character creator developed and implemented in the example

--- ReflectiveRoomManager/Assets/ReflectiveRoomManager/Example/Basic_AdditiveRoom/Scripts/NETWORK/Room/RoomPlayerSpawn.cs
+++ ReflectiveRoomManager/Assets/ReflectiveRoomManager/Example/Basic_AdditiveRoom/Scripts/NETWORK/Room/RoomPlayerSpawn.cs
@@ -1,8 +1,7 @@
 ﻿using Mirror;
 using UnityEngine;
-using System.Collections;
 using REFLECTIVE.Runtime.NETWORK.Room;
-using REFLECTIVE.Runtime.NETWORK.Utilities;
+using REFLECTIVE.Runtime.NETWORK.Player.Utilities;
 
 namespace Example.Basic.Network.Room
 {
@@ -29,112 +28,22 @@ namespace Example.Basic.Network.Room
             if (_useLobby)
                 RoomManagerBase.Singleton.Events.OnServerExitedRoom += CreateLobbyPlayer;
             else
-                RoomManagerBase.Singleton.Events.OnServerExitedRoom += RemovePlayer;
+                RoomManagerBase.Singleton.Events.OnServerExitedRoom += PlayerCreatorUtilities.RemovePlayer;
         }
-
-        /// <summary>
-        /// Performs player creation for the game.
-        /// If there is an identity, it deletes it and creates a new one.
-        /// </summary>
-        /// <param name="conn"></param>
+        
         private void CreateGamePlayer(NetworkConnection conn)
         {
-            if (conn.identity != null)
-            {
-                StartCoroutine(ReplacePlayer(conn, _gamePlayerPrefab));
+            var player = PlayerCreatorUtilities.TryCreatePlayerOrReplace(conn, _gamePlayerPrefab);
 
-                return;
-            }
-
-            CreatePlayer(conn, _gamePlayerPrefab);
-        }
-
-        /// <summary>
-        /// When the player leaves the room,
-        /// he deletes his object and loads the lobby prefab.
-        /// </summary>
-        /// <param name="conn"></param>
-        private void CreateLobbyPlayer(NetworkConnection conn)
-        {
-            if (conn.identity == null) return;
-
-            StartCoroutine(ReplacePlayer(conn, _lobbyPlayerPrefab));
-        }
-
-        /// <summary>
-        /// For the given connection, it deletes the old player object and creates a new one
-        /// </summary>
-        /// <param name="conn"></param>
-        /// <param name="prefab"></param>
-        /// <returns></returns>
-        private IEnumerator ReplacePlayer(NetworkConnection conn, GameObject prefab)
-        {
-            var oldPlayer = conn.identity.gameObject;
-            
-            var newPlayer = SpawnPlayer(conn, prefab);
-
-            NetworkServer.ReplacePlayerForConnection(conn.identity.connectionToClient, newPlayer, true);
-
-            yield return new WaitForEndOfFrame();
-            
-            if (newPlayer.TryGetComponent(out SimpleCharacterController controller))
-            {
-                controller.ID = conn.connectionId;
-            }
-            
-            if(oldPlayer != null)
-                NetworkServer.Destroy(oldPlayer);
-        }
-
-        /// <summary>
-        /// Creates the specified prefab as a player object for the given connection
-        /// </summary>
-        /// <param name="conn"></param>
-        /// <param name="prefab"></param>
-        private void CreatePlayer(NetworkConnection conn, GameObject prefab)
-        {
-            if (conn is not NetworkConnectionToClient connectionToClient) return;
-            
-            var player = SpawnPlayer(conn, prefab);
-
-            NetworkServer.AddPlayerForConnection(connectionToClient, player);
-            
             if (player.TryGetComponent(out SimpleCharacterController controller))
             {
                 controller.ID = conn.connectionId;
             }
         }
-
-        private void RemovePlayer(NetworkConnection conn)
-        {
-            if (conn is not NetworkConnectionToClient connectionToClient) return;
-
-            var player = connectionToClient.identity.gameObject;
-            
-            NetworkServer.RemovePlayerForConnection(conn, false);
-            
-            NetworkServer.Destroy(player);
-        }
         
-        /// <summary>
-        /// It tries to find the scene of the room via connection.
-        /// Creates the specified prefab in the given scene.
-        /// </summary>
-        /// <param name="conn"></param>
-        /// <param name="prefab"></param>
-        /// <returns></returns>
-        private GameObject SpawnPlayer(NetworkConnection conn, GameObject prefab)
+        private void CreateLobbyPlayer(NetworkConnection conn)
         {
-            var room = RoomManagerBase.Singleton.GetRoomOfPlayer(conn);
-
-            //If there is no room, then it is in the lobby, and since it is in the lobby in this script,
-            //I take the scene of its own object.
-            //If you need it to go to a different scene, you can change it.
-            var scene = room?.Scene ?? gameObject.scene;
-            
-            var player = NetworkSpawnUtilities.SpawnObjectForScene(scene, prefab, Vector3.zero, Quaternion.identity);
-
-            return player;
+            PlayerCreatorUtilities.ReplacePlayer(conn, _lobbyPlayerPrefab);
         }
     }
 }
---
+++ ReflectiveRoomManager/Assets/ReflectiveRoomManager/Scripts/Runtime/NETWORK/Player/Utilities/PlayerCreatorUtilities.cs
@@ -0,0 +1,93 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace REFLECTIVE.Runtime.NETWORK.Player.Utilities
+{
+    using Room;
+    using NETWORK.Utilities;
+    
+    public static class PlayerCreatorUtilities
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conn"></param>
+        /// <param name="prefab"></param>
+        public static GameObject TryCreatePlayerOrReplace(NetworkConnection conn, GameObject prefab)
+        {
+            return conn.identity != null
+                ? ReplacePlayer(conn, prefab)
+                : CreatePlayer(conn, prefab);
+        }
+        
+        /// <summary>
+        /// Creates the specified prefab as a player object for the given connection
+        /// </summary>
+        /// <param name="conn"></param>
+        /// <param name="prefab"></param>
+        public static GameObject CreatePlayer(NetworkConnection conn, GameObject prefab)
+        {
+            if (conn is not NetworkConnectionToClient connectionToClient) return null;
+            
+            var player = SpawnPlayer(conn, prefab);
+
+            NetworkServer.AddPlayerForConnection(connectionToClient, player);
+
+            return player;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conn"></param>
+        /// <param name="prefab"></param>
+        public static GameObject ReplacePlayer(NetworkConnection conn, GameObject prefab)
+        {
+            if (conn.identity == null) return null;
+            
+            var oldPlayer = conn.identity.gameObject;
+            
+            var newPlayer = SpawnPlayer(conn, prefab);
+
+            NetworkServer.ReplacePlayerForConnection(conn.identity.connectionToClient, newPlayer, true);
+            
+            if(oldPlayer != null)
+                NetworkServer.Destroy(oldPlayer);
+
+            return newPlayer;
+        }
+
+        public static void RemovePlayer(NetworkConnection conn)
+        {
+            if (conn is not NetworkConnectionToClient connectionToClient) return;
+
+            var player = connectionToClient.identity.gameObject;
+            
+            NetworkServer.RemovePlayerForConnection(conn, false);
+            
+            NetworkServer.Destroy(player);
+        }
+
+        /// <summary>
+        /// It tries to find the scene of the room via connection.
+        /// Creates the specified prefab in the given scene.
+        /// </summary>
+        /// <param name="conn"></param>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        private static GameObject SpawnPlayer(NetworkConnection conn, GameObject prefab)
+        {
+            var room = RoomManagerBase.Singleton.GetRoomOfPlayer(conn);
+
+            //If there is no room, then it is in the lobby, and since it is in the lobby in this script,
+            //I take the scene of its own object.
+            //If you need it to go to a different scene, you can change it.
+            var scene = room?.Scene ?? SceneManager.GetActiveScene();
+            
+            var player = NetworkSpawnUtilities.SpawnObjectForScene(scene, prefab, Vector3.zero, Quaternion.identity);
+
+            return player;
+        }
+    }
+}
