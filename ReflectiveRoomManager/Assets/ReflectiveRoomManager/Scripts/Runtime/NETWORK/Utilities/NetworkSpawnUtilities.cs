26e4f7be1c43cb14ce6050e61464de6b1d59a4dc
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2023-12-06 01:37:59 +0300

    NetworkSpawnUtilities has been updated
    Functions made more useful

--- ReflectiveRoomManager/Assets/ReflectiveRoomManager/Scripts/Runtime/NETWORK/Utilities/NetworkSpawnUtilities.cs
+++ ReflectiveRoomManager/Assets/ReflectiveRoomManager/Scripts/Runtime/NETWORK/Utilities/NetworkSpawnUtilities.cs
@@ -1,13 +1,14 @@
 ﻿using Mirror;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.SceneManagement;
 
 namespace REFLECTIVE.Runtime.NETWORK.Utilities
 {
     public static class NetworkSpawnUtilities
     {
-        public static GameObject SpawnObject(GameObject obj, Vector3 position = default, Quaternion rotation = default)
+        public static GameObject SpawnObject(GameObject obj, Vector3 position, Quaternion rotation, NetworkConnection conn)
         {
             if (NetworkManager.singleton == null)
             {
@@ -16,14 +17,39 @@ namespace REFLECTIVE.Runtime.NETWORK.Utilities
             }
 
             var instantObj = Object.Instantiate(obj, position, rotation);
-            NetworkServer.Spawn(instantObj);
+            NetworkServer.Spawn(instantObj, conn);
+
+            return instantObj;
+        }
+        
+        public static GameObject SpawnObject(GameObject obj, NetworkConnection conn)
+        {
+            if (NetworkManager.singleton == null)
+            {
+                Debug.LogWarning("Network Manager is null", obj);
+                return null;
+            }
+
+            var instantObj = Object.Instantiate(obj, Vector3.zero, Quaternion.identity);
+            NetworkServer.Spawn(instantObj, conn);
 
             return instantObj;
         }
 
-        public static GameObject SpawnObjectForScene(Scene scene, GameObject obj, Vector3 position = default, Quaternion rotation = default)
+        public static GameObject SpawnObjectForScene(Scene scene, GameObject obj, NetworkConnection conn = null)
         {
-            var spawnedObj = SpawnObject(obj, position, rotation);
+            var spawnedObj = SpawnObject(obj, default, default, conn);
+
+            if (spawnedObj == null) return null;
+            
+            SceneManager.MoveGameObjectToScene(spawnedObj, scene);
+
+            return spawnedObj;
+        }
+        
+        public static GameObject SpawnObjectForScene(Scene scene, GameObject obj, Vector3 position, Quaternion rotation, NetworkConnection conn = null)
+        {
+            var spawnedObj = SpawnObject(obj, position, rotation, conn);
 
             if (spawnedObj == null) return null;
             
@@ -34,7 +60,12 @@ namespace REFLECTIVE.Runtime.NETWORK.Utilities
         
         public static IEnumerable<GameObject>  GetSpawnablePrefabs()
         {
-            var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
+            var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
+
+            foreach (var prefab in spawnablePrefabs.ToArray().Where(prefab => prefab == NetworkManager.singleton.playerPrefab))
+            {
+                spawnablePrefabs.Remove(prefab); 
+            }
 
             return spawnablePrefabs;
         }
