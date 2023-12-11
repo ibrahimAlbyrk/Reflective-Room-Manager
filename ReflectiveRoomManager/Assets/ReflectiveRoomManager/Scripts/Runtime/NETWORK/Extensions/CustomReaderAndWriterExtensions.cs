20e5b06cd89b98a4c52d13959cfa9b371491a995
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2023-12-11 13:12:39 +0300

    Serialization update
    Custom readers and writers were written for the dictionary.

---
+++ ReflectiveRoomManager/Assets/ReflectiveRoomManager/Scripts/Runtime/NETWORK/Extensions/CustomReaderAndWriterExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Mirror;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace REFLECTIVE.Runtime.NETWORK.Extensions
+{
+    public static class CustomReaderAndWriterExtensions
+    {
+        public static void WriteDictionary(this NetworkWriter writer, Dictionary<string, string> dictionary)
+        {
+            List<string> keys = null;
+            List<string> values = null;
+
+            if (dictionary != null)
+            {
+                keys = dictionary.Keys.ToList();
+                values = dictionary.Values.ToList();
+            }
+
+            writer.WriteList(keys);
+            writer.WriteList(values);
+        }
+
+        public static Dictionary<string, string> ReadDictionary(this NetworkReader reader)
+        {
+            var keys = reader.ReadList<string>();
+            var values = reader.ReadList<string>();
+
+            var dict = new Dictionary<string, string>();
+
+            if (keys == null || values == null) return dict;
+
+            for (var i = 0; i < keys.Count; i++)
+            {
+                dict[keys[i]] = values[i];
+            }
+
+            return dict;
+        }
+    }
+}
