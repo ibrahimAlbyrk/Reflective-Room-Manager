2fc48c89ad33f2e3ec3b61571d8bf94daac5e9de
ibrahimAlbayrak <ibrahimalbayrak812@gmail.com>
2024-03-15 01:06:56 +0300

    EditorHelper has been added

---
+++ ReflectiveRoomManager/Assets/ReflectiveRoomManager/Scripts/Editor/EditorHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+
+namespace REFLECTIVE.Editor
+{
+    public static class EditorHelper
+    {
+        public static IEnumerable<string> IterateOverProject(string filter)
+        {
+            foreach (string guid in AssetDatabase.FindAssets(filter))
+            {
+                yield return AssetDatabase.GUIDToAssetPath(guid);
+            }
+        }
+    }
+}
