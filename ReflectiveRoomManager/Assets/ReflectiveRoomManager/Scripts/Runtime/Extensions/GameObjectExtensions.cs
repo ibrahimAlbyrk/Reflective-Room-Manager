f87eb42587db93f30167a35a91c35f86503c15c3
ibrahim albayrak <ibrahimalbayrak812@gmail.com>
2026-02-02 21:08:27 +0300

    fix(ext): allocate RoomContainerHelper per call
    Shared static instance caused cross-scene state leaks
    when multiple GameObjects queried RoomContainer.

--- ReflectiveRoomManager/Assets/ReflectiveRoomManager/Scripts/Runtime/Extensions/GameObjectExtensions.cs
+++ ReflectiveRoomManager/Assets/ReflectiveRoomManager/Scripts/Runtime/Extensions/GameObjectExtensions.cs
@@ -6,8 +6,6 @@ namespace REFLECTIVE.Runtime.Extensions
     
     public static class GameObjectExtensions
     {
-        private static RoomContainerHelper _roomContainerHelper;
-        
         /// <summary>
         /// Extension method that returns a RoomContainerHelper object for a given GameObject.
         /// </summary>
@@ -15,16 +13,7 @@ namespace REFLECTIVE.Runtime.Extensions
         /// <returns>A RoomContainerHelper object.</returns>
         public static RoomContainerHelper RoomContainer(this GameObject gameObject)
         {
-            if (_roomContainerHelper == null)
-            {
-                _roomContainerHelper = new RoomContainerHelper(gameObject.scene);
-            }
-            else
-            {
-                _roomContainerHelper.Scene = gameObject.scene;
-            }
-            
-            return _roomContainerHelper;
+            return new RoomContainerHelper(gameObject.scene);
         }
     }
 }
